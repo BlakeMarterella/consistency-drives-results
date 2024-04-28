0fc09b3897480aea57b151bf243537c551dd54d7
Blake M <blake@marterella.com>
2024-04-28 16:57:29 -0400

    Create proof of concept home page

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { HomeViewRoutingModule } from './home-routing.module';
-// import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SharedModule } from '@shared/shared.module';
 
 
 @NgModule({
   declarations: [
-
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
-    HomeViewRoutingModule
+    HomeViewRoutingModule,
+    SharedModule
   ]
 })
 export class HomeModule { }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,39 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
 	"./src/**/*.{html,ts}",
   ],
   theme: {
+    fontFamily: {
+      sans: ['Public Sans', ...defaultTheme.fontFamily.sans],
+      // sans: ['Nunito Sans', 'sans-serif'],
+      // serif: ['Merriweather', 'serif'],
+    },
     extend: {
       colors: {
         'black': {
           DEFAULT: "#04080F"
         },
+        'navy-blue': {
+          DEFAULT: "#21437C"
+        },
         'main-blue': {
           DEFAULT: "#507DBC",
-          0: "#6363AC",
+          1: "#6363AC",
         },
         'secondary-blue': {
           DEFAULT: "#A1C6EA",
-          0: "#B1B1E1"
+          1: "#B1B1E1"
         },
         'accent-blue': {
             DEFAULT: "#BBD1EA",
-            0: "#C4C4E3"
+            1: "#C4C4E3"
         },
         'secondary-accent-blue': {
             DEFAULT: "#DAE3E5",
-            0: "DDDDE4"
+            1: "DDDDE4"
         }
       }
     },
